64658f1f3c7303e2ce5451bef2dcc544df718d6a
leopheard <leopheard.misc@gmail.com>
2019-08-26 19:29:22 -0400

    upload

--- addon.py
+++ addon.py
@@ -2,15 +2,16 @@ from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
 
 plugin = Plugin()
-
 URL1 = "https://rss.art19.com/stephanie-millers-happy-hour-podcast"
 URL2 = "https://next.podbay.fm/podcast/1458666669"
+URL3 = "http://www.am950radio.com/category/podcasts/atheists-talk/feed/"
+URL4 = "http://www.am950radio.com/category/podcasts/accomplishers/feed/"
+URL5 = "http://www.am950radio.com/category/podcasts/best-of-interviews/feed/"
+URL6 = "http://weactradio.libsyn.com/rss"
+URL7 = "https://www.bradblog.com/podcastgen/bradcast/feed.xml"
 
 @plugin.route('/')
 def main_menu():
-    """
-    main menu 
-    """
     items = [
         {
             'label': plugin.get_string(10000), 
@@ -25,52 +26,77 @@ def main_menu():
             'label': plugin.get_string(30002), 
             'path': plugin.url_for('all_episodes2'),
             'thumbnail': "https://is5-ssl.mzstatic.com/image/thumb/Music123/v4/14/18/f0/1418f032-425f-8d31-9473-f310edf32d4b/source/400x400bb.jpg"},
+        {
+            'label': plugin.get_string(30003), 
+            'path': plugin.url_for('all_episodes3'),
+            'thumbnail': "http://www.am950radio.com/wp-content/uploads/2017/09/atheists.jpg"},
+        {
+            'label': plugin.get_string(30004), 
+            'path': plugin.url_for('all_episodes4'),
+            'thumbnail': "http://www.am950radio.com/wp-content/uploads/2019/04/MISSION-ACCOMPLISHERS-large-only-for-itunes-dont-use.jpg"},
+        {
+            'label': plugin.get_string(30005), 
+            'path': plugin.url_for('all_episodes5'),
+            'thumbnail': "http://www.am950radio.com/wp-content/uploads/2018/04/Best-of-1400.jpg"},
+        {
+            'label': plugin.get_string(30006), 
+            'path': plugin.url_for('all_episodes6'),
+            'thumbnail': "https://ssl-static.libsyn.com/p/assets/8/a/b/1/8ab1668ee74fb2c0/logo-print_NEW.jpg"},
+        {
+            'label': plugin.get_string(30006), 
+            'path': plugin.url_for('all_episodes6'),
+            'thumbnail': "http://bradblog.com/podcastgen/bradcast/images/itunes_image.jpg"},
     ]
-
     return items
-
 
 @plugin.route('/all_episodes1/')
 def all_episodes1():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup1 = mainaddon.get_soup1(URL1)
-    
     playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
-    
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
-
     return items
-
 
 @plugin.route('/all_episodes2/')
 def all_episodes2():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup2 = mainaddon.get_soup2(URL2)
-    
     playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
-    
     items = mainaddon.compile_playable_podcast2(playable_podcast2)
-
     return items
-
 
 @plugin.route('/all_episodes3/')
 def all_episodes3():
-    """
-    contains playable podcasts listed as just-in
-    """
     soup3 = mainaddon.get_soup3(URL3)
-    
     playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
-    
     items = mainaddon.compile_playable_podcast3(playable_podcast3)
-
     return items
 
+@plugin.route('/all_episodes4/')
+def all_episodes4():
+    soup4 = mainaddon.get_soup4(URL4)
+    playable_podcast4 = mainaddon.get_playable_podcast4(soup4)
+    items = mainaddon.compile_playable_podcast4(playable_podcast4)
+    return items
+
+@plugin.route('/all_episodes5/')
+def all_episodes5():
+    soup5 = mainaddon.get_soup5(URL5)
+    playable_podcast5 = mainaddon.get_playable_podcast3(soup5)
+    items = mainaddon.compile_playable_podcast5(playable_podcast5)
+    return items
+
+@plugin.route('/all_episodes6/')
+def all_episodes6():
+    soup6 = mainaddon.get_soup6(URL6)
+    playable_podcast6 = mainaddon.get_playable_podcast6(soup6)
+    items = mainaddon.compile_playable_podcast6(playable_podcast6)
+    return items
+
+@plugin.route('/all_episodes7/')
+def all_episodes7():
+    soup7 = mainaddon.get_soup6(URL7)
+    playable_podcast7 = mainaddon.get_playable_podcast7(soup7)
+    items = mainaddon.compile_playable_podcast6(playable_podcast7)
+    return items
 
 if __name__ == '__main__':
     plugin.run()
--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -3,128 +3,110 @@ import re
 from bs4 import BeautifulSoup
 
 def get_soup1(url1):
-    """
-    @param: url of site to be scraped
-    """
     page = requests.get(url1)
     soup1 = BeautifulSoup(page.text, 'html.parser')
-    
-    print "type: ", type(soup1)
+    print("type: ", type(soup1))
     return soup1
-
 get_soup1("https://rss.art19.com/stephanie-millers-happy-hour-podcast")
 
 def get_soup2(url2):
-    """
-    @param: url of site to be scraped
-    """
     page = requests.get(url2)
     soup2 = BeautifulSoup(page.text, 'html.parser')
-    
-    print "type: ", type(soup2)
+    print("type: ", type(soup2))
     return soup2
-
 get_soup2("https://next.podbay.fm/podcast/1458666669")
 
+def get_soup3(url3):
+    page = requests.get(url3)
+    soup3 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup3))
+    return soup3
+get_soup3("http://www.am950radio.com/wp-content/uploads/2017/09/atheists.jpg")
+
+def get_soup4(url4):
+    page = requests.get(url4)
+    soup4 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup4))
+    return soup4
+get_soup4("http://www.am950radio.com/category/podcasts/accomplishers/feed/")
+
+def get_soup5(url5):
+    page = requests.get(url5)
+    soup5 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup5))
+    return soup5
+get_soup5("http://www.am950radio.com/category/podcasts/best-of-interviews/feed/")
+
+def get_soup6(url6):
+    page = requests.get(url6)
+    soup6 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup6))
+    return soup6
+get_soup6("http://weactradio.libsyn.com/rss")
+
+def get_soup7(url7):
+    page = requests.get(url7)
+    soup7 = BeautifulSoup(page.text, 'html.parser')
+    print("type: ", type(soup7))
+    return soup7
+get_soup7("https://www.bradblog.com/podcastgen/bradcast/feed.xml")
 
 def get_playable_podcast1(soup1):
-    """
-    @param: parsed html page            
-    """
     subjects = []
-
     for content in soup1.find_all('item'):
-        
         try:        
             link = content.find('enclosure')
             link = link.get('url')
-            print "\n\nLink: ", link
-
+            print("\n\nLink: ", link)
             title = content.find('title')
             title = title.get_text()
-
-#            desc = content.find('description')
-#            desc = desc.get_text()
-
             thumbnail = content.find('itunes:image')
             thumbnail = thumbnail.get('href')
-
         except AttributeError:
             continue
-              
         item = {
                 'url': link,
                 'title': title,
-#                'desc': desc,
                 'thumbnail': thumbnail
         }
-        
-        subjects.append(item) 
-    
+        subjects.append(item)
     return subjects
-
-
 def compile_playable_podcast1(playable_podcast1):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast1:
         items.append({
             'label': podcast['title'],
             'thumbnail': podcast['thumbnail'],
             'path': podcast['url'],
-#            'info': podcast['desc'],
             'is_playable': True,
     })
-
     return items
 
-
 def get_playable_podcast2(soup2):
-    """
-    @param: parsed html page            
-    """
     subjects = []
-
     for content in soup2.find_all('body'):
-        
         try:        
             link = content.find('a')
             link = link.get('href')
-            print "\n\nLink: ", link
-
+            print("\n\nLink: ", link)
             title = content.find('div', {'class': 'jsx-460898420 title'})
             title = title.get_text()
-
-            desc = content.find('div', {'class': 'jsx-460898420 description'})
-            desc = desc.get_text()
-
+#            desc = content.find('div', {'class': 'jsx-460898420 description'})
+#            desc = desc.get_text()
             thumbnail = content.find('div', {'class': 'jsx-460898420 download'})
             thumbnail = thumbnail.get('href')
-
         except AttributeError:
-            continue
-              
+            continue             
         item = {
                 'href': link,
                 'title': title,
-                'desc': desc,
+#                'desc': desc,
                 'thumbnail': thumbnail,
         }
-        
-        subjects.append(item) 
-    
+        subjects.append(item)
     return subjects
-
-
 def compile_playable_podcast2(playable_podcast2):
-    """
-    @para: list containing dict of key/values pairs for playable podcasts
-    """
     items = []
-
     for podcast in playable_podcast2:
         items.append({
             'label': podcast['title'],
@@ -133,5 +115,159 @@ def compile_playable_podcast2(playable_podcast2):
             'info': podcast['desc'],
             'is_playable': True,
     })
+    return items
 
+def get_playable_podcast3(soup3):
+    subjects = []
+    for content in soup3.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast3(playable_podcast3):
+    items = []
+    for podcast in playable_podcast3:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast4(soup4):
+    subjects = []
+    for content in soup4.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast4(playable_podcast4):
+    items = []
+    for podcast in playable_podcast4:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast5(soup5):
+    subjects = []
+    for content in soup5.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast5(playable_podcast5):
+    items = []
+    for podcast in playable_podcast5:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast6(soup6):
+    subjects = []
+    for content in soup6.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast6(playable_podcast6):
+    items = []
+    for podcast in playable_podcast6:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
+    return items
+
+def get_playable_podcast7(soup7):
+    subjects = []
+    for content in soup7.find_all('item'):
+        try:        
+            link = content.find('enclosure')
+            link = link.get('url')
+            print("\n\nLink: ", link)
+            title = content.find('title')
+            title = title.get_text()
+            thumbnail = content.find('itunes:image')
+            thumbnail = thumbnail.get('href')
+        except AttributeError:
+            continue
+        item = {
+                'url': link,
+                'title': title,
+                'thumbnail': thumbnail
+        }
+        subjects.append(item)
+    return subjects
+def compile_playable_podcast7(playable_podcast7):
+    items = []
+    for podcast in playable_podcast7:
+        items.append({
+            'label': podcast['title'],
+            'thumbnail': podcast['thumbnail'],
+            'path': podcast['url'],
+            'is_playable': True,
+    })
     return items
